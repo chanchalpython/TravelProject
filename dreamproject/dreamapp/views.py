187d1fc6dc863f68839cf9b1323d010d4bd85115
chanchal <krishnachanchal23@gmail.com>
2021-11-17 21:52:19 +0530

    added

---
+++ dreamproject/dreamapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place
+from . models import team
+# Create your views here.
+def demo(request):
+    obj=place.objects.all()
+    obj1=team.objects.all()
+    return render(request,"index.html",{'result':obj,'results':obj1})
+def task(request):
+    return render(request,"about.html")
